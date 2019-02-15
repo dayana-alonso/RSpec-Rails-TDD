108d88a1667c2fe01eb1081e1577a0e7e38432e4
Dayana <dayanalon83@gmail.com>
2019-02-15 16:32:34 +0000

    models validation -ok

--- spec/features/tasks/tasks_creates_spec.rb
+++ spec/features/tasks/tasks_creates_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks::Creates", type: :feature do
-	describe 'creating tasks' do
-		before(:each) do
-			visit new_task_path
-		end
+describe 'creating tasks' do
+	before(:each) do
+		visit new_task_path
+	end
 
-		scenario 'valid task' do 
-			expect {
-				fill_in :task_name, with: "learn to code with kingsley"
+	scenario 'valid task' do 
+		expect {
+			fill_in :task_name, with: "learn to code with kingsley"
 
-				click_button 'save task'
+			click_button 'save task'
 			}.to change(Task, :count).by(1)
 
-			expect(page).to have_xpath(dt('fash-notice'))
-			expect(page).to have_current_path(tasks_path)
-		end
-		scenario 'invalid task' do
+		expect(page).to have_xpath(dt('fash-notice'))
+		expect(page).to have_current_path(tasks_path)
+	end
+	
+	scenario 'invalid task' do
+		expect {
+			fill_in :task_name, with:""
 
+			click_button 'save task'
+			}.to change(Task, :count).by(0)
+
+			expect(page).to have_xpath(dt('fash-notice'))
+			expect(page).to have_xpath(dt('validation-errors'))
 		end
 	end
 end
